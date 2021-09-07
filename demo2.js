b8c6f606dc57da272c15ca1ac9393296faad1dd3
Supit_Edgard <69880569+Edgardcoding@users.noreply.github.com>
2021-09-07 09:15:25 +0700

    Add files via upload

---
+++ demo2.js
@@ -0,0 +1,9 @@
+import * as name from "./modules/modul1.js";
+
+const stu = new name.Student('105011810036', 'FIK');
+console.log(stu.display());
+
+
+console.log(name.PII);
+
+console.log(name.hello('Edgard'));
